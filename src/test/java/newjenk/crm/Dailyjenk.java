7fa7f0012025ffb1131113d8be1ce20ff4cf90b2
PC-1 <PC-1@DESKTOP-K0ITVV0>
2022-03-28 13:09:09 +0530

    first  commit

---
+++ src/test/java/newjenk/crm/Dailyjenk.java
@@ -0,0 +1,10 @@
+package newjenk.crm;
+
+import org.testng.annotations.Test;
+
+public class Dailyjenk {
+@Test
+public void main() {
+	System.out.println("demo for practise");
+}
+}
---
+++ src/test/java/newjenk/crm/package-info.java
@@ -0,0 +1,1 @@
+package newjenk.crm;
